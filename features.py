d06b9866babb12c91c1eb38afed8457a9abf4d5c
Antoine A <59095838+Piazo@users.noreply.github.com>
2022-10-10 13:38:55 +0200

    create avatar

---
+++ features.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+languageList = ['romanian', 'swedish', 'maltese', 'belgian', 'luxembourgish', 
+                'dutch', 'french', 'finnish', 'austrian', 'slovakian', 'hungarian', 
+                'bulgarian', 'danish', 'greek', 'croatian', 'polish', 'german', 
+                'spanish', 'estonian', 'lithuanian', 'cypriot', 'latvian', 'irish', 
+                'italian', 'slovene', 'czech', 'portuguese']
+
+cityList = ['vilnius', 'paris', 'valletta', 'madrid', 'amsterdam', 
+            'copenhagen', 'rome', 'sofia', 'vienna']
+
+mobileList = [0, 1]
+
+dateList = np.linspace(44,0,45)
--- makeRequest.py
+++ makeRequest.py
@@ -1,14 +1,19 @@
 import urllib.parse
 import requests
+import pandas as pd
+import features
+
+print(features.cityList, features.languageList)
 
 domain = "51.91.251.0"
 port = 3000
 host = f"http://{domain}:{port}"
 path = lambda x: urllib.parse.urljoin(host, x)
 
-user_id = '18fcee0f-416b-4fd4-8fce-58c7d2030f43'
-name = 'first_avatar'
-r = requests.post(path(f'avatars/{user_id}/{name}'))
+def create_avatar(nameAvatar):
+    user_id = '18fcee0f-416b-4fd4-8fce-58c7d2030f43'
+    name = nameAvatar
+    r = requests.post(path(f'avatars/{user_id}/{name}'))
 
 
 
