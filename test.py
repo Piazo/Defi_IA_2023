14ec2164b53ad48ed679bc11fd294e24e6daf538
Antoine A <59095838+Piazo@users.noreply.github.com>
2022-10-10 12:39:19 +0200

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv('./test_set.csv')
+priceList = pd.read_csv('./sample_submission.csv')['price'].tolist()
+# print(priceList)
+df.reset_index
+
+
+for index, row in df.iterrows():
+    df.loc[index, 'price'] = priceList[df.loc[index, 'hotel_id']]
+# print(df["price"].tolist)
+
+
+df2=df.groupby(['city'])['price'].agg("mean")
+print(df2)
