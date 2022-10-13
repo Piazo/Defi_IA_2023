b124334559ed19eb4233b74c2719a964848671df
MatthieuQcc <matthieu.quaccia@gmail.com>
2022-10-13 10:15:00 +0200

    changement tab request

--- test.py
+++ test.py
@@ -17,11 +17,11 @@ tab_request = [
                 ["Avataricard01", 'cypriot', 'madrid', 44, 0],
                 ["Avataricard01", 'cypriot', 'paris', 44, 1],
 
-                ["Avataricard01", 'danish', 'rome', 44, 1],
-                ["Avataricard01", 'danish', 'sofia', 44, 0],
+                ["Avataricard01", 'danish', 'rome', 20, 1],
+                ["Avataricard01", 'danish', 'sofia', 20, 0],
 
-                ["Avataricard01", 'dutch', 'rome', 44, 0],
-                ["Avataricard01", 'dutch', 'sofia', 44, 1],
+                ["Avataricard01", 'dutch', 'rome', 20, 0],
+                ["Avataricard01", 'dutch', 'sofia', 20, 1],
 
                 ["Avataricard01", 'estonian', 'valletta', 20, 0],
                 ["Avataricard01", 'estonian', 'vienna', 20, 1],
@@ -47,16 +47,16 @@ tab_request = [
                 ["Avataricard01", 'luxembourgish', 'vilnius', 20, 0],
                 ["Avataricard01", 'luxembourgish', 'sofia', 20, 1],
 
-                ["Avataricard01", 'maltese', 'amsterdam', 20, 0],
-                ["Avataricard01", 'maltese', 'copenhagen', 20, 1],
+                ["Avataricard01", 'maltese', 'amsterdam', 12, 0],
+                ["Avataricard01", 'maltese', 'copenhagen', 12, 1],
 
-                ["Avataricard01", 'polish', 'madrid', 20, 0],
-                ["Avataricard01", 'polish', 'paris', 20, 1],
+                ["Avataricard01", 'polish', 'madrid', 12, 0],
+                ["Avataricard01", 'polish', 'paris', 12, 1],
 
-                ["Avataricard01", 'portuguese', 'rome', 20, 0],
-                ["Avataricard01", 'portuguese', 'valletta', 20, 1],
+                ["Avataricard01", 'portuguese', 'rome', 12, 0],
+                ["Avataricard01", 'portuguese', 'valletta', 12, 1],
 
-                ["Avataricard01", 'romanian', 'vienna', 20, 0],
+                ["Avataricard01", 'romanian', 'vienna', 12, 0],
                 ["Avataricard01", 'romanian', 'vilnius', 12, 1],
 
                 ["Avataricard01", 'slovakian', 'amsterdam', 12, 0],
